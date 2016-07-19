8d9c0413724afedea3c5b54d9e2a69520c75830a
Renguang Dong <dongrenguang@gmail.com>
2016-07-19 16:48:01 +0800

    Complete View, ViewController, Application and Application for the framework.

---
+++ src/sap/a/view/View.js
@@ -0,0 +1,135 @@
+import ManagedObject from "sap/ui/base/ManagedObject";
+
+export default class View extends ManagedObject
+{
+    metadata = {
+        aggregations: {
+            subviews: {
+                type: "sap.a.view.View"
+            }
+        },
+        events: {
+            addedToParent: { }
+        }
+    };
+
+    constructor(...args)
+    {
+        super(...args);
+        this.afterInit();
+    }
+
+    init()
+    {
+        this.$element = $(`<${this.getElementTag()} />`);
+        if (this.id !== null)
+        {
+            this.$element.attr("id", this.getId());
+        }
+        this.$container = this.$element;
+    }
+
+    afterInit()
+    {
+
+    }
+
+    getElementTag()
+    {
+        return "div";
+    }
+
+    addStyleClass(...args)
+    {
+        this.$element.addClass(...args);
+    }
+
+    removeStyleClass(...args)
+    {
+        this.$element.removeClass(...args);
+    }
+
+    toggleStyleClass(...args)
+    {
+        this.$element.toggleClass(...args);
+    }
+
+    show(...args)
+    {
+        this.$element.show(...args);
+    }
+
+    hide(...args)
+    {
+        this.$element.hide(...args);
+    }
+
+    toggle(...args)
+    {
+        this.$element.toggle(...args);
+    }
+
+
+
+
+    addSubview(subview, $container = this.$container)
+    {
+        if (subview.getParent())
+        {
+            subview.removeFromParent();
+        }
+        this.addAggregation("subviews", subivew);
+        subview.placeAt($container);
+        this.fireAddedToParent();
+        return this;
+    }
+
+    removeSubview(subview, neverUseAgain = false)
+    {
+        const result = this.removeAggregation("subviews", subview);
+        if (result)
+        {
+            if (neverUseAgain)
+            {
+                subview.$element.remove();
+            }
+            else
+            {
+                subview.$element.detach();
+            }
+        }
+        return result;
+    }
+
+    removeAllSubviews(neverUseAgain)
+    {
+        while (this.getSubviews().length > 0)
+		{
+			this.removeSubview(this.getSubviews()[0], neverUseAgain);
+		}
+    }
+
+    removeFromParent()
+    {
+        if (this.getParent())
+        {
+            this.getParent().removeSubview(this);
+        }
+    }
+
+    placeAt(target)
+    {
+        const $target = (target instanceof jQuery ? target : $(target));
+        $target.append(this.$element);
+    }
+
+    $(...args)
+    {
+        return this.$element.find(...args);
+    }
+
+    toString()
+    {
+        return `${this.getMetadata().getName()}[${this.getId()}]`;
+    }
+}
